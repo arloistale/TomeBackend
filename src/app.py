9ab1e7f4ecbc791dc8d20f15b9a0548b1f86c69e
Jonathan Lu <jonathanlu321@gmail.com>
2023-09-13 13:46:20 -0400

    Typo

--- src/app.py
+++ src/app.py
@@ -24,7 +24,7 @@ def create_app():
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[ALLOWED_ORIGINS],
+        allow_origins=ALLOWED_ORIGINS,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
